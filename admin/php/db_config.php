8003d5dac1e7767c5c0611f3f86ae62818b073d4
Corderslite <abrahamgreatebele@gmail.com>
2025-07-04 22:45:03 +0100

    update

--- admin/php/db_config.php
+++ admin/php/db_config.php
@@ -2,9 +2,9 @@
 $live = true;
 // $live = false;
 $servername = "localhost";
-$username = $live?"premvssx_news":"root";
-$password = $live?"Newspointcenter":"root";
-$dbname = $live?"premvssx_news":"anchored";
+$username = $live?"leatviuo_anchored":"root";
+$password = $live?"Anchoredchaplainsllc":"root";
+$dbname = $live?"leatviuo_anchored":"anchored";
 $con = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
